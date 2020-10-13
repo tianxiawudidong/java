49a3ae0238b6f7c9e80d6be5db8b3f8adc8fc49f
xudongjun <dongjun.xu@ifchange.com>
2020-10-13 15:40:14 +0800

    判断单链表是否有环

---
+++ src/main/java/com/txwdd/lettcode/LinkCycleTest.java
@@ -0,0 +1,44 @@
+package com.txwdd.lettcode;
+
+/**
+ * 判断链表是否有环
+ */
+public class LinkCycleTest {
+
+    private static class ListNode{
+        private ListNode next;
+        int value;
+        public ListNode(int value) {
+            this.value = value;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode node = new ListNode(1);
+        node.next = new ListNode(2);
+        node.next.next = new ListNode(3);
+        node.next.next.next = new ListNode(4);
+        node.next.next.next.next = new ListNode(5);
+        //创建环路
+        node.next.next.next.next.next = node;
+        boolean flag = hasCircle(node);
+        System.out.println(flag);
+    }
+
+    //判断链表是否为有环链表
+    private static boolean hasCircle(ListNode node){
+        ListNode s = node;
+        ListNode f = node;
+        while(true){
+            //一次一两步
+            s = s.next;
+            //一次走两步
+            f = f.next.next;
+            System.out.println("s:"+s.value+"-->"+"f:"+f.value);
+            //假如是有环链表那么返回true
+            if(s == f)return true;
+            //假如是无环链表那么返回false
+            if(f.next == null) return false;
+        }
+    }
+}
---
+++ src/main/java/com/txwdd/lettcode/TwoArrCompact.java
@@ -0,0 +1,42 @@
+package com.txwdd.lettcode;
+
+import java.util.Arrays;
+
+/**
+ * 两个有序数组合并
+ */
+public class TwoArrCompact {
+
+    private static int[] merge(int[] a, int b[]) {
+
+        int[] result = new int[a.length + b.length];
+        //i: a数组 j:b数组 k:result数组
+        int i = 0, j = 0, k = 0;
+        while (i < a.length && j < b.length) {
+            if (a[i] < b[j]) {
+                result[k++] = a[i];
+                i++;
+            } else {
+                result[k++] = b[j];
+                j++;
+            }
+        }
+
+        /* 后面连个while循环是用来保证两个数组比较完之后剩下的一个数组里的元素能顺利传入 */
+        while (i < a.length) {
+            result[k++] = a[i++];
+        }
+        while (j < b.length) {
+            result[k++] = b[j++];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] a={1,2,3,4,5,6};
+        int[]b={3,4,5,6,7,8,9,10};
+        int[] c = merge(a, b);
+        System.out.println(Arrays.toString(c));
+    }
+}
